d358bde8830acd121b2999c09c942c67441ea9c9
ttiimmaa123 <ttiimmuurr2007@gmail.com>
2026-02-17 23:06:34 +0300

    Add a complete working XSS scanner in xss_scanner.py

--- xss_scanner.py
+++ xss_scanner.py
@@ -1,11 +1,36 @@
-# XSS Scanner
+import requests
 
-# Your XSS scanner code here
+# List of common XSS payloads
+xss_payloads = [
+    "<script>alert(1)</script>",
+    "<img src=x onerror=alert(1)>",
+    "<svg/onload=alert(1)>",
+    "<iframe src='javascript:alert(1)'></iframe>",
+    "<a href='javascript:alert(1)'>click me</a>",
+]
 
-# Example function with a syntax error for demonstration
+def test_xss(url):
+    for payload in xss_payloads:
+        # Sending the payload to the URL
+        response = requests.get(url + payload)
+        if payload in response.text:
+            print(f'XSS Vulnerability found with payload: {payload} at {url}')
+        else:
+            print(f'No XSS vulnerability found with payload: {payload} at {url}')
 
-def example_function():
-    # Original line with syntax error
-    print('"<script>alert("XSS6")</script>")
+def scan(target):
+    # Check if target is a valid URL, IP, or domain
+    if not target.startswith('http'):
+        target = 'http://' + target
+    print(f'Scanning target: {target}\n')
+    test_xss(target)
 
-# Other code...
+if __name__ == '__main__':
+    targets = [
+        'http://example.com/',
+        'http://test.com/',
+        '192.168.1.1',
+        'http://mydomain.com/',
+    ]
+    for target in targets:
+        scan(target)
